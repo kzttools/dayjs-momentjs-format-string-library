dddcbb8ea8324f242dbf16f7d92cd157d4821f36
KazooTTT <31075337+KazooTTT@users.noreply.github.com>
2023-09-28 07:31:40 +0000

    feat：default export convert to separately

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
-const time24Hour = 'HH:mm:ss';
+// Date format: Year-Month-Day
+export const formatDate = "YYYY-MM-DD";
 
-export default {
-    // Date format: Year-Month-Day
-    date: "YYYY-MM-DD",
+// 24-hour time format: Hour:Minute:Second
+export const formatTime24Hour = 'HH:mm:ss';
 
-    // 24-hour time format: Hour:Minute:Second
-    time: time24Hour,
+// Same 24-hour time format
+export const formatTime = formatTime24Hour;
 
-    // Same 24-hour time format
-    time24Hour: time24Hour,
+// 12-hour time format: Hour:Minute:Second AM/PM
+export const formatTime12Hour = 'hh:mm:ss A';
 
-    // 12-hour time format: Hour:Minute:Second AM/PM
-    time12Hour: 'hh:mm:ss A',
+// Full day of the week format: Full name of the day (e.g., Monday)
+export const formatDayOfWeekFull = "dddd";
 
-    // Full day of the week format: Full name of the day (e.g., Monday)
-    dayOfWeekFull: "dddd",
-
-    // Date format with separator: Year-Month-Day, separator specified as a parameter
-    dateWithSeparator: (separator: string) => "YYYY-MM-DD".replace("-", separator),
-};
+// Date format with separator: Year-Month-Day, separator specified as a parameter
+export const formatDateWithSeparator = (separator: string) => "YYYY-MM-DD".replace("-", separator);
